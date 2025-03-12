9619fd6975dcb88d0edc0826270a8c79a886e575
Rohit Singh <rohit.singh11@ibm.com>
2025-03-12 20:00:21 +0530

    added

---
+++ packages/container/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+const App=()=>{
+
+    return (
+        <div className='App'>
+          <h1>Hello React.</h1>
+          <MarketingApp />
+        </div>
+      );
+}
+
+export default App
+
