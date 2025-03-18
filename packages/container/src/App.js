b423db9224ac810b892a4ade5836f69feea2b0fe
Rohit Singh <rohit.singh11@ibm.com>
2025-03-18 18:42:51 +0530

    added ch

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ const App=()=>{
 
     return (
         <div className='App'>
-          <h1>Hello React.</h1>
+          <h1>Hello React.!!!!!!</h1>
           <MarketingApp />
         </div>
       );
