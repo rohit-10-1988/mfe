97679983dd39417ba9164c4f26beb1f395717b91
Rohit Singh <rohit.singh11@ibm.com>
2025-03-18 19:48:19 +0530

    second change for marketting

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -14,7 +14,7 @@ const prodConfig = {
       name: 'container',
       filename: 'remoteEntry.js',
       remotes: {
-        marketing:`marketing@${domain}marketing/latest/remoteEntry.js`,
+        marketing:`marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       
     }),
